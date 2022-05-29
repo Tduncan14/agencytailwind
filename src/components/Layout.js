2d664afd4bb2ae6647976ba8c8582a10a8404592
tduncan14 <nbatre1@yahoo.com>
2022-05-28 22:33:07 -0700

    keep moving forward

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,31 +1,33 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
 
 
 
 function Layout({children}){
+
+    const location = useLocation()
 
 
     const menuItems = [
         {
         title:'Home',
         path:'/',
-        icon:''
+        icon:'ri-home-smile-line'
         },
         {
           title:'About',
           path:'/about',
-          icon:'',
+          icon:'ri-information-line',
         },
         {
             title:'Clients',
             path:'/client',
-            icon:''
+            icon:'ri-user-location-fill'
         },
         {
             title:'Contact',
             path:'/contact',
-            icon:''
+            icon:'ri-contacts-fill'
         }
     ]
 
@@ -43,9 +45,17 @@ function Layout({children}){
             
             <div className="flex  w-full justify-center">
                 {
-                    menuItems.map((item,i) =>(
-         <div className={`px-20 bg-primary py-5 ${i === 0 && 'rounded-l'} ${i === menuItems.length-1 && 'rounded-r'}`} key={i}>
-                       <Link className="text-secondary" to={item.path}>{item.title}</Link>
+                    menuItems.map((item,index) =>(
+                        <div className="flex flex-col"> 
+
+                       {location.pathname === item.path && (<div>
+                           <h1>Active</h1>
+                       </div>)}
+         <div className={`px-20 bg-primary py-5 ${index === 0 && 'rounded-l'} ${index === menuItems.length-1 && 'rounded-r'}flex items-center justify-center space-x-5`} key={index}>
+                      
+                      <i className={`${item.icon} text-secondary text-xl`}></i>
+                       <Link className="text-secondary text-xl" to={item.path}>{item.title}</Link>
+                        </div>
                         </div>
                     ))
                 }
